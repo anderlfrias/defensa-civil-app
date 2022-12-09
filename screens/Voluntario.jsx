5aa1a66bf98efdf374e6fe4092b71f8f026d93cc
anderlfrias <anderl.friaas@gmail.com>
2022-12-09 16:07:20 -0400

    Registro de voluntario, listo

--- screens/Voluntario.jsx
+++ screens/Voluntario.jsx
@@ -1,110 +1,194 @@
-import { View, Text, TextInput, TouchableOpacity } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Keyboard, TouchableWithoutFeedback, ScrollView } from 'react-native'
 import React, {useState} from 'react'
+import { globalStyles } from '../utils/globalStyles';
 
 const Voluntario = () => {
     const [datos, setDatos] = useState({});
+    const [isSending, setIsSending] = useState(false);
 
     const sudmit = (data) => {
+        if (!validateFields()) {
+            alert("Debe completar todos los campos");
+            return;
+        }
+
+        setIsSending(true);
+
         const formData = new FormData();
-        // formData.append('cedula', data.cedula);
-        // formData.append('nombre', data.nombre);
-        // formData.append('apellido', data.apellido);
-        // formData.append('telefono', data.telefono);
-        // formData.append('correo', data.correo);
-        // formData.append('clave', data.clave);
-        formData.append('json', JSON.stringify(data));
+        formData.append('cedula', data.cedula);
+        formData.append('nombre', data.nombre);
+        formData.append('apellido', data.apellido);
+        formData.append('telefono', data.telefono);
+        formData.append('correo', data.correo);
+        formData.append('clave', data.clave);
 
         console.log(formData);
 
         fetch('https://adamix.net/defensa_civil/def/registro.php', {
             method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
             body: formData
         })
             .then((response) => response.json())
             .then((responseJson) => {
                 console.log(responseJson);
+                alert(responseJson.mensaje);
+                if (responseJson.exito) {
+                    setDatos({});
+                }
             })
             .catch((error) => {
                 console.error("Error:", error);
-            }
-        );
+            })
+            .finally(() => {
+                setIsSending(false)
+            });
     }
+
+    const validateFields = () => {
+        if (datos.cedula && datos.nombre && datos.apellido && datos.telefono && datos.correo && datos.clave) {
+            return true;
+        }
+        return false;
+    };
     return (
-        <View>
-            <View>
-                <View>
-                    <Text>Form title</Text>
-                    <Text>Form subtitle</Text>
-                </View>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+            <ScrollView>
+            <View style={styles.container}>
+                <View style={styles.form}>
+                    <View style={styles.formHeader}>
+                        <Text style={styles.formTitle}>Formulario de Voluntarios</Text>
+                        <Text style={styles.formSubtitle}>Complete todos los campos para procesar su solicitud</Text>
+                    </View>
 
-                <View>
-                    <Text>Cedula</Text>
-                    <TextInput
-                        placeholder="ej. 123456789"
-                        onChangeText={(text) => setDatos({...datos, cedula: text})}
-                        value={datos.cedula}
-                    />
-                </View>
+                    <View style={styles.inputContainer}>
+                        <Text style={styles.label}>Cedula</Text>
+                        <TextInput
+                            placeholder="ej. 123456789"
+                            onChangeText={(text) => setDatos({...datos, cedula: text})}
+                            value={datos.cedula}
+                            style={styles.input}
+                        />
+                    </View>
 
-                <View>
-                    <Text>Nombre</Text>
-                    <TextInput
-                        placeholder="ej. Juan"
-                        onChangeText={(text) => setDatos({...datos, nombre: text})}
-                        value={datos.nombre}
-                    />
-                </View>
+                    <View style={styles.inputContainer}>
+                        <Text style={styles.label}>Nombre</Text>
+                        <TextInput
+                            placeholder="ej. Juan"
+                            onChangeText={(text) => setDatos({...datos, nombre: text})}
+                            value={datos.nombre}
+                            style={styles.input}
+                        />
+                    </View>
 
-                <View>
-                    <Text>Apellido</Text>
-                    <TextInput
-                        placeholder="ej. Perez"
-                        onChangeText={(text) => setDatos({...datos, apellido: text})}
-                        value={datos.apellido}
-                    />
-                </View>
+                    <View style={styles.inputContainer}>
+                        <Text style={styles.label}>Apellido</Text>
+                        <TextInput
+                            placeholder="ej. Perez"
+                            onChangeText={(text) => setDatos({...datos, apellido: text})}
+                            value={datos.apellido}
+                            style={styles.input}
+                        />
+                    </View>
 
-                <View>
-                    <Text>Telefono</Text>
-                    <TextInput
-                        placeholder="ej. 123456789"
-                        onChangeText={(text) => setDatos({...datos, telefono: text})}
-                        value={datos.telefono}
-                    />
-                </View>
+                    <View style={styles.inputContainer}>
+                        <Text style={styles.label}>Telefono</Text>
+                        <TextInput
+                            placeholder="ej. 123456789"
+                            onChangeText={(text) => setDatos({...datos, telefono: text})}
+                            value={datos.telefono}
+                            style={styles.input}
+                        />
+                    </View>
 
-                <View>
-                    <Text>Correo electronico</Text>
-                    <TextInput
-                        placeholder="ej. tunombre@example.es"
-                        onChangeText={(text) => setDatos({...datos, correo: text})}
-                        value={datos.correo}
-                    />
-                </View>
+                    <View style={styles.inputContainer}>
+                        <Text style={styles.label}>Correo electronico</Text>
+                        <TextInput
+                            placeholder="ej. tunombre@example.es"
+                            onChangeText={(text) => setDatos({...datos, correo: text})}
+                            value={datos.correo}
+                            style={styles.input}
+                        />
+                    </View>
 
-                <View>
-                    <Text>Contraseña</Text>
-                    <TextInput
-                        placeholder="********"
-                        onChangeText={(text) => setDatos({...datos, clave: text})}
-                        value={datos.clave}
-                    />
-                </View>
+                    <View style={styles.inputContainer}>
+                        <Text style={styles.label}>Contraseña</Text>
+                        <TextInput
+                            placeholder="********"
+                            onChangeText={(text) => setDatos({...datos, clave: text})}
+                            value={datos.clave}
+                            style={styles.input}
+                            secureTextEntry={true}
+                        />
+                    </View>
 
-                <View>
-                    <TouchableOpacity
-                        onPress={() => sudmit(datos)}
-                    >
-                        <Text>Registrarse</Text>
-                    </TouchableOpacity>
+                    <View>
+                        <TouchableOpacity
+                            onPress={() => sudmit(datos)}
+                            style={styles.button}
+                            disabled={isSending}
+                        >
+                            <Text
+                                style={styles.buttonText}
+                            >
+                                {isSending ? 'Enviando...' : 'Enviar'}
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </View>
-        </View>
+            </ScrollView>
+        </TouchableWithoutFeedback>
     )
 }
 
-export default Voluntario
+export default Voluntario;
+
+const styles = StyleSheet.create({
+    container: {
+        ...globalStyles.container,
+    },
+    form: {
+        ...globalStyles.card,
+        flex: 1,
+        width: '100%',
+    },
+    formHeader: {
+        marginBottom: 20,
+    },
+    formTitle: {
+        ...globalStyles.title,
+        fontSize: 25,
+        fontWeight: 'bold',
+    },
+    formSubtitle: {
+        ...globalStyles.subtitle,
+        fontSize: 15,
+    },
+    inputContainer: {
+        marginBottom: 20,
+    },
+    label: {
+        ...globalStyles.text,
+        fontSize: 16,
+    },
+    input: {
+        ...globalStyles.text,
+        fontSize: 16,
+        borderWidth: 1,
+        borderColor: '#A1A0A5',
+        borderRadius: 5,
+        padding: 10,
+    },
+    button: {
+        ...globalStyles.bgColorPrimary,
+        padding: 10,
+        borderRadius: 5,
+        marginTop: 20,
+    },
+    buttonText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#fff',
+        textAlign: 'center',
+    },
+});
---
+++ utils/globalStyles.js
@@ -0,0 +1,37 @@
+const globalStyles = {
+    container: {
+        alignItems: 'center',
+        backgroundColor: '#F8F7FA',
+        flex: 1,
+        padding: 20,
+    },
+    title: {
+        color: '#4D4B55',
+    },
+    text: {
+        color: '#75737B',
+    },
+    subtitle: {
+        color: '#A1A0A5',
+    },
+    card: {
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        padding: 20,
+        // shadow
+            // shadowColor: "#000",
+            // shadowOffset: {
+            //     width: 0,
+            //     height: 1,
+            // },
+            // shadowOpacity: 0.20,
+            // shadowRadius: 1.41,
+
+            // elevation: 5,
+    },
+    bgColorPrimary: {
+        backgroundColor: '#3b82f6',
+    },
+};
+
+export { globalStyles };
